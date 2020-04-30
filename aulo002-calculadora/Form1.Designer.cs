0ce0cbe36138b9af47e268407c4b3d46c4dac318
Diogo Costa <diogotj@gmail.com>
2020-04-30 20:02:24 -0300

    aula 002
    aula 002

--- aulo002-calculadora/Form1.Designer.cs
+++ aulo002-calculadora/Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboOperacao = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(33, 54);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 20);
+            this.txtValor1.TabIndex = 0;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(224, 54);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(100, 20);
+            this.txtValor2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor 2";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(30, 138);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(33, 96);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(243, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cboOperacao);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.txtValor1);
+            this.groupBox1.Controls.Add(this.lblResultado);
+            this.groupBox1.Controls.Add(this.txtValor2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(526, 266);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculadora";
+            // 
+            // cboOperacao
+            // 
+            this.cboOperacao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOperacao.FormattingEnabled = true;
+            this.cboOperacao.Items.AddRange(new object[] {
+            "Somar",
+            "Subtrair",
+            "Multiplicar",
+            "Dividir"});
+            this.cboOperacao.Location = new System.Drawing.Point(140, 54);
+            this.cboOperacao.Name = "cboOperacao";
+            this.cboOperacao.Size = new System.Drawing.Size(78, 21);
+            this.cboOperacao.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Operação";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(546, 286);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboOperacao;
     }
 }
 
--- aulo002-calculadora/Form1.cs
+++ aulo002-calculadora/Form1.cs
@@ -16,5 +16,78 @@ namespace aulo002_calculadora
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //se for verdade
+            if ( string.IsNullOrWhiteSpace( txtValor1.Text ) )
+            {
+                //condicao verdadeira
+                MessageBox.Show("Você precisa digitar o valor 1",
+                    "Erro", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                    );
+
+                //foco no controle
+                txtValor1.Focus();
+
+                //Não continuar
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtValor2.Text))
+            {
+                //condicao verdadeira
+                MessageBox.Show("Você precisa digitar o valor 2",
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                    );
+
+                //foco no controle
+                txtValor2.Focus();
+
+                //Não continuar
+                return;
+            }
+            if ( string.IsNullOrWhiteSpace( cboOperacao.Text) ) 
+            {
+                MessageBox.Show("Você precisa selecionar a operação",
+                "Erro",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning
+                );
+
+                cboOperacao.Focus();
+
+                return;
+            }
+
+            int numero1 = Convert.ToInt32( txtValor1.Text );
+            int numero2 = Convert.ToInt32( txtValor2.Text );
+
+
+            if (cboOperacao.Text == "Somar")
+            {
+                lblResultado.Text = Convert.ToString(numero1 + numero2);
+            }
+            else if (cboOperacao.Text == "Subtrair") 
+            {
+                lblResultado.Text = Convert.ToString(numero1 - numero2);
+            }
+            else if (cboOperacao.Text == "Multiplicar") 
+            {
+                lblResultado.Text = Convert.ToString(numero1 * numero2);
+            }
+            else
+            {
+                //dividir
+                lblResultado.Text = Convert.ToString(numero1 / numero2);
+            }
+
+                                           
+            
+            //lblResultado.Text =  (numero1 + numero2).ToString();
+        }
     }
 }
